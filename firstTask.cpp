b17f833ea8d79d37a17811457e706212070f24d3
Kirill <126481207+Bob-Jacka@users.noreply.github.com>
2025-06-04 11:10:48 +0400

    Change space count in function
    Долбанный windows блокнот добавляет по 8 пробелом при табуляции!!

--- firstTask.cpp
+++ firstTask.cpp
@@ -2,10 +2,12 @@
 
 using namespace std;
 
-void counting_function() {
-	static int funcCount = 1;
-	cout <<  "Количество вызовов функции counting_function(): " << funcCount << endl;
-	funcCount++;
+static int funcCount = 1;
+
+void counting_function() 
+{
+    cout <<  "Количество вызовов функции counting_function(): " << funcCount << endl;
+    funcCount++;
 }
 
 int main(int argc, char** argv)
